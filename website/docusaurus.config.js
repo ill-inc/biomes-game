2f759f3f35b600c560d183999af4310c0009671c
DevinLeamy <devinleamy@gmail.com>
2023-08-14 12:44:57 -0400

    Adjusted the theme

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   url: "https://biomes.gg",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/website/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
