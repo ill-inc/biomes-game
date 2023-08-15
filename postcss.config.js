ad6f40e39d46f570a2a361c693d02e38bd369a35
DevinLeamy <devinleamy@gmail.com>
2023-08-15 14:04:44 -0400

    Fixed docs styling without impacting the game's styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {
-      // Required for Docusaurus: https://github.com/facebook/docusaurus/issues/6631#issuecomment-1033469556
-      // content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
