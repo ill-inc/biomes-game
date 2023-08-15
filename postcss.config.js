eabcc37aa0574f989bd46d4181a1192bb2c0129c
DevinLeamy <devinleamy@gmail.com>
2023-08-15 00:39:44 -0400

    Fix styles (#15)
    * Very hot fix
    
    * Very hot fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     tailwindcss: {
-      // Required for Docusaurus: https://github.com/facebook/docusaurus/issues/6631https://github.com/facebook/docusaurus/issues/6631#issuecomment-1033469556
-      content: ["./src/**/*.{html,js}"],
+      // Required for Docusaurus: https://github.com/facebook/docusaurus/issues/6631#issuecomment-1033469556
+      // content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
     },
     autoprefixer: {},
   },
